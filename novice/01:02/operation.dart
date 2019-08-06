db9bba19198389351f893bbb4e02f8d51383d80b
Akmal Maulana <47621190+kmalmaulana@users.noreply.github.com>
2019-08-06 20:16:43 +0700

    Latihan Operator
    Latihan operator pada Dart

---
+++ novice/01:02/operation.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a, b, c, d, e, f, g, h;
+
+  a = 5;
+  b = 30000;
+  c = 55000;
+
+  // here we go
+  d = a * b * 27;
+  print('d = $d');
+
+  e = a * 50;
+  print('e = $e');
+
+  f = d - 20000;
+  print('f = $f');
+
+  g = e % 10;
+  print('g = $g');
+}
